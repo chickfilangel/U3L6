b9308c04f5567a6b377501693a4aea0b08cd4d1e
chickfilangel <43784681+chickfilangel@users.noreply.github.com>
2018-12-05 12:38:54 -0500

    Add files via upload

---
+++ lesson 6/problem2.py
@@ -0,0 +1,15 @@
+name = input ('What is your name? ')
+
+print('Nice to meet you ' + name + ', my name is Chatbox.') 
+
+age = input ('How old are you? ')
+
+print('Wow ' + age + ' years old! I was born today! ')
+
+feeling = input ('How are you feeling today? ')
+
+print('Glad to hear you are feeling ' + feeling.lower() + ' that is wonderful to hear!')
+
+food = input('What food are you craving right now? ')
+
+print('Me too!')
---
+++ lesson 6/problem4.py
@@ -0,0 +1,11 @@
+print('Adding Bot')
+
+print('Follow my directions. ')
+
+x = input('2')
+
+y = input('12')
+
+result = x + y
+
+print(result)
